1ad8374fe24789922f828d603d25f1649d160a1f
Gnailliang <mingliang.dingl@gmail.com>
2022-10-12 22:42:47 +0800

    修改文件目录错误

--- demo.py
+++ demo.py
@@ -6,9 +6,10 @@ global ypath
 
 def texts():
     global ypath
+    print("将一组16进制数每4个字节分组并转成10进制数\n")
     ypath = input("请输入需要处理的文件：")
     infile = open(ypath, "rb")
-    outfile = open(os.path.dirname(ypath)+"2.txt", "w")
+    outfile = open(os.path.dirname(ypath)+"\\tempHEX.txt", "w")
     for eachline in infile.readlines():
         lines = str(eachline, encoding="utf-8").split(' ')
         n = 0
@@ -25,8 +26,8 @@ def texts():
 
 def textz():
     global ypath
-    infile = open(os.path.dirname(ypath)+"2.txt", "rb")
-    outfile = open(os.path.dirname(ypath)+"3.txt", "w")
+    infile = open(os.path.dirname(ypath)+"\\tempHEX.txt", "rb")
+    outfile = open(os.path.dirname(ypath)+"/result.txt", "w")
     for eachline in infile.readlines():
         temp = str(int(str(eachline, encoding="utf-8"), 16))
         print(temp)
